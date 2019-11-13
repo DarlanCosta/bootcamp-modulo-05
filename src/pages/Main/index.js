28eacc07596cf3bc5c396ecd24abe35594dfc9c0
Darlan Costa <darlandsv@gmail.com>
2019-11-12 22:46:45 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+function Main() {
+  return (
+    <Title error>
+      Main <small>Oloco</small>
+    </Title>
+  );
+}
+
+export default Main;
