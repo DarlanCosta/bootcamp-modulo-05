bf869b301b32768ca2814f6d7917ec56efb8e3a3
DarlanCosta <darlandsv@gmail.com>
2019-11-14 10:51:05 -0300

    2019-11-14

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FaGithubAlt } from 'react-icons/fa';
 
 import { Container } from './styles';
 
 export default function Main() {
   return (
     <Container>
-      <h1>Repositórios</h1>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
     </Container>
   );
 }
