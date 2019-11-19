8bfdceab74f7fd4987426df997f70b750fa0c06a
DarlanCosta <darlandsv@gmail.com>
2019-11-19 18:03:07 -0300

    t

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
+import Container from '../../components/Container';
 
 import { Loading } from './styles';
 
